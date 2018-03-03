3d20c806a259624880ba64305f0c286051ba19f0
Artur Kaleta <49493036+Abuseware@users.noreply.github.com>
2018-03-03 18:38:20 +0100

    Removed interpolation and numpy/scipy dependencies - interpolation for 100k records consumes over 50GiB of memory

--- render.py
+++ render.py
@@ -3,38 +3,29 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as md
 
-import numpy
-import scipy.interpolate as interp
-
 import csv
 import datetime
+import array
 
 #Configuration variables
 yaxis_label = '\u2103'
 xaxis_format = md.DateFormatter('%d.%m.%Y %H:%M')
-#length_divider = 240
-interpolation_multiplier = 10
-
 
 #Load data from file
-data = numpy.loadtxt('temp.csv', delimiter = ';', dtype = {'names': ['timestamp', 'degrees'], 'formats': ['int32', 'f']} , skiprows = 1)
+timestamps = array.array('i')
+degrees = array.array('d')
+with open('temp.csv') as f:
+	f.readline()
+	data = csv.reader(f, delimiter = ';')
 
-#Parse data into arrays
-timestamps = numpy.array([float(x[0]) for x in data])
-degrees = numpy.array([float(x[1]) for x in data])
-
-#Interpolate
-samples = len(timestamps) * interpolation_multiplier
-print("Interpolating {} samples to {} samples".format(len(timestamps), samples))
-xnew = numpy.linspace(timestamps.min(), timestamps.max(), samples)
-ynew = interp.spline(timestamps, degrees, xnew)
+	for row in data:
+		timestamps.append(int(row[0]))
+		degrees.append(float(row[1]))
 
 #Render plot
-#print("Output width: {}in".format(len(data)/length_divider))
-#plt.figure(figsize=(len(data)/length_divider, 8))
 plt.figure(figsize=(14, 8))
 
-plt.plot([datetime.datetime.fromtimestamp(x) for x in xnew], ynew)
+plt.plot([datetime.datetime.fromtimestamp(x) for x in timestamps], degrees)
 #plt.plot([datetime.datetime.fromtimestamp(x) for x in timestamps], degrees, linestyle='none', marker='o', markersize=0.5)
 
 plt.ylabel(yaxis_label)
