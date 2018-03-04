e20016a0df08a5fa299f7ead7459824086217796
Artur Kaleta <hg.licho@mailhero.io>
2018-03-04 12:01:28 +0100

    Added parameters for specifying image size

--- render.py
+++ render.py
@@ -25,6 +25,8 @@ parser = argparse.ArgumentParser(description='Render thermometer data into plot.
 parser.add_argument('file', type=argparse.FileType(), help='source file')
 parser.add_argument('--format', choices=['svg', 'png'], default='svg', help='destination image format')
 parser.add_argument('--markers', action='store_true', help='draw markers on samples')
+parser.add_argument('--width', type=int, default=35, help='output width in centimeters')
+parser.add_argument('--height', type=int, default=20, help='output height in centimeters')
 parser.add_argument('--days', type=int, default=0, help='render data from last DAYS')
 parser.add_argument('--hours', type=int, default=0, help='render data from last HOURS')
 parser.add_argument('--minutes', type=int, default=0, help='render data from last MINUTES')
@@ -57,7 +59,7 @@ if not len(timestamps):
     exit(1)
 
 # Render plot
-plt.figure(figsize=(14, 8))
+plt.figure(figsize=(args.width * 0.3937, args.height * 0.3937))
 
 plt.plot([datetime.datetime.fromtimestamp(x) for x in timestamps], degrees)
 if args.markers:
