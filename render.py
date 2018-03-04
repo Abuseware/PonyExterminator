30c6037a3fbdeae2c353d8b28a7d2535acad9ee5
Artur Kaleta <hg.licho@mailhero.io>
2018-03-04 11:01:00 +0100

    Error handling, support commandline parameters.

--- render.py
+++ render.py
@@ -1,32 +1,67 @@
 #!/usr/bin/env python
 
-import matplotlib.pyplot as plt
-import matplotlib.dates as md
+import sys
+import argparse
 
 import csv
-import datetime
 import array
+
+import datetime
+import time
+
+try:
+	import matplotlib.pyplot as plt
+	import matplotlib.dates as md
+except (ImportError, ModuleNotFoundError):
+	print("Matplotlib not found, please install required libraries")
+	exit(1)
 
 #Configuration variables
 yaxis_label = '\u2103'
 xaxis_format = md.DateFormatter('%d.%m.%Y %H:%M')
 
+#Parse command line arguments
+parser = argparse.ArgumentParser(description = 'Render thermometer data into plot.')
+parser.add_argument('file', type = argparse.FileType(), help = 'source file')
+parser.add_argument('--format', choices = ['svg', 'png'], default = 'svg', help = 'destination image format')
+parser.add_argument('--markers', action = 'store_true', help = 'draw markers on samples')
+parser.add_argument('--days', type = int, default = 0, help = 'render data from last DAYS')
+parser.add_argument('--hours', type = int, default = 0, help = 'render data from last HOURS')
+parser.add_argument('--minutes', type = int, default = 0, help = 'render data from last MINUTES')
+parser.add_argument('--seconds', type = int, default = 0, help = 'render data from last SECONDS')
+
+args = parser.parse_args(sys.argv[1:])
+
+#Calculate delta timestamp
+time_delta = (args.days * 24 * 60 * 60) + (args.hours * 60 * 60) + (args.minutes * 60) + args.seconds
+time_after = int(time.time()) - time_delta if time_delta else 0
+
 #Load data from file
 timestamps = array.array('i')
 degrees = array.array('d')
-with open('temp.csv') as f:
-	f.readline()
-	data = csv.reader(f, delimiter = ';')
+try:
+	args.file.readline() #Skip header
+	for row in csv.reader(args.file, delimiter = ';'):
+		if int(row[0]) > time_after:
+			timestamps.append(int(row[0]))
+			degrees.append(float(row[1]))
+except ValueError:
+	print("File malformed - value has wrong data type, exiting.")
+	exit(1)
+except IndexError:
+	print("File malformed - wrong number of columns, exiting.")
+	exit(1)
 
-	for row in data:
-		timestamps.append(int(row[0]))
-		degrees.append(float(row[1]))
+if not len(timestamps):
+	print("No data available in specified time range, or file is empty.")
+	exit(1)
 
 #Render plot
 plt.figure(figsize=(14, 8))
 
 plt.plot([datetime.datetime.fromtimestamp(x) for x in timestamps], degrees)
-#plt.plot([datetime.datetime.fromtimestamp(x) for x in timestamps], degrees, linestyle='none', marker='o', markersize=0.5)
+if args.markers:
+	plt.plot([datetime.datetime.fromtimestamp(x) for x in timestamps], degrees, linestyle='none', marker='o', markersize=3)
 
 plt.ylabel(yaxis_label)
 
@@ -35,4 +70,11 @@ plt.xticks(fontsize=8, rotation=10)
 plt.grid(linestyle='dashed')
 
 #Save plot to file
-plt.savefig("plot.svg", dpi='figure')
+try:
+	plt.savefig(args.file.name.rsplit('.')[0] + '.' + args.format, dpi='figure')
+except PermissionError:
+	print("Cannot write to file, exiting.")
+	exit(1)
+except Exception:
+	print("Unexpected error: " + sys.exc_info()[0])
+	raise
